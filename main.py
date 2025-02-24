afa69eb28a5ab5af0c7051225ffdd9ab812566db
giuliafig <giuliafigliomeni7@gmail.com>
2025-02-24 02:37:21 +0000

    s

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"} 
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+@app.get("/ping")
+async def ping():
+    return {"status": "ok", "message": "pong"} 
